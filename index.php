346e80281f23d610aa4b70e6345e7d911fa065cf
Ben <benjamin.bagot.05@gmail.com>
2015-03-06 17:31:26 +0100

    Structure + Templates
    Add architecture of repo + template base

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Beerpong</title>
+	<link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+
+	<script type="text/javascript" src="lib/jquery-2.1.3.min.js"></script>
+	<script type="text/javascript" src="lib/bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+	<?php include "partials/header.php" ?>
+	<div id="content" class="col-xs-6 col-xs-offset-3">
+
+	</div>
+</body>
+</html>
+
